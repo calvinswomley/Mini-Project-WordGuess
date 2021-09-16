b69972366dab9094a2bc5e8c6e3ab9796e349894
Calvin Swomley <calvinswomley@gmail.com>
2021-09-15 21:29:40 -0600

    Javascript file outline

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 
-//start game button listener
+
+
+
+
+//start game by clicking the button
+var startButton = document.querySelector("#start-button")
 startButton.addEventListener("click", ourFunction())
 
 //guess word by filling in blanks that match the number of letters in word
 
 
+
+
 //game timer
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        sendMessage();
+      }
+    }, 1000);
+  }
+
+
+
